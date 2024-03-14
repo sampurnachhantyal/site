efaa36e1bc039dc30fefe182683a4571934eee0d
Amol Shrestha <amol@eventlogic.se>
2024-03-14 23:10:40 +0545

    Blog page and dummy content added

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'Threadcode Tech';
 export const SITE_DESCRIPTION = "From cutting-edge software development to seamless network integration, unlock the power of technology with Threadcode. Let's code your success together."
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
