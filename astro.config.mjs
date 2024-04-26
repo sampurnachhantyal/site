e1a50a3f79d6570458a4050452a9bd916e567ae9
Amol Shrestha <amol@eventlogic.se>
2024-04-26 19:24:50 +0545

    Base url changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://sampurnachhantyal.github.io',
-  base: 'site'
+  site: 'https://info.threadcodetech.com'
 
 });
